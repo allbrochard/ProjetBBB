8273eb5ae8220adc0cfaeeaeecb2b09c896f7f5d
allan <allan@allan-PC.home>
2017-01-15 15:59:44 +0100

    pile

--- big boss band/src/Projet/Main.java
+++ big boss band/src/Projet/Main.java
@@ -8,24 +8,25 @@ public class Main {
 
 	public static void main(String[] args) {
 
-		JFrame login = new JFrame(){
+		JFrame login = new JFrame();
 
-			public void Login(){
+		
+		
+		login.setSize(300,150);
+		login.setDefaultCloseOperation(0);
+		JPanel pan1= new JPanel();
+		JButton bouton = new JButton(" LOGIN ");
+		JTextField txuser = new JTextField(15);
 
-				setSize(300,150);
-				setDefaultCloseOperation(EXIT_ON_CLOSE);
-				JPanel pan1= new JPanel();
-				JButton bouton = new JButton(" LOGIN ");
-				JTextField txuser = new JTextField(15);
-
-				pan1.add(bouton);
-				pan1.add(txuser);
-				this.setContentPane(pan1);
-				this.setVisible(true);
-				pan1.setVisible(true);
-				System.out.println("salut");
-			}
-		};
+		pan1.add(bouton);
+		pan1.add(txuser);
+		login.setContentPane(pan1);
+		login.setVisible(true);
+		pan1.setVisible(true);
+		
+	
 	}
+	
+
 
 }
