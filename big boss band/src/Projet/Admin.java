5ea81462bdce979c11e924abcd1cfa2e9857d3af
allan <allan@allan-PC.home>
2017-01-15 15:33:15 +0100

    modif admin

--- big boss band/src/Projet/Admin.java
+++ big boss band/src/Projet/Admin.java
@@ -5,7 +5,7 @@ public class Admin extends Compte {
 
 	public Admin(String login, String mp, String nom, String prenom, int age) {
 		super(login, mp, nom, prenom, age);
-		// TODO Auto-generated constructor stub
+		
 	}
 
 	public void creeCompte(){
