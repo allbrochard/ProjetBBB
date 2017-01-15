3ea8625488bfa2aa8aa76787ad61e22941f5b8ac
allan <allan@allan-PC.home>
2017-01-15 16:22:33 +0100

    dernière mise a jour du main fonctionnel de la mort qui tue qui souleve des daronne

--- big boss band/src/Projet/Main.java
+++ big boss band/src/Projet/Main.java
@@ -1,14 +1,10 @@
 package Projet;
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.JTextField;
 
 public class Main {
 
 	public static void main(String[] args) {
 
-		
+		Graphique lol = new Graphique();
 	}
 
 }
