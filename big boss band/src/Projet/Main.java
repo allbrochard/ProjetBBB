21d92db2d1079b324b78baea53924577d669401f
allan <allan@allan-PC.home>
2017-01-15 13:53:26 +0100

    MODIF salle et main

--- big boss band/src/Projet/Main.java
+++ big boss band/src/Projet/Main.java
@@ -5,20 +5,25 @@ import javax.swing.JPanel;
 import javax.swing.JTextField;
 
 public class Main {
-	JFrame login = new JFrame(){
+	public static void main(String[] args) {
 
-	public void Login(){
+		JFrame login = new JFrame(){
 
-	setSize(300,150);
-	setDefaultCloseOperation(EXIT_ON_CLOSE);
-	JPanel pan1= new JPanel();
-	JButton bouton = new JButton(" LOGIN ");
-	JTextField txuser = new JTextField(15);
-	pan1.add(bouton);
-	pan1.add(txuser);
-	this.setContentPane(pan1);
-	this.setVisible(true);
+			public void Login(){
 
+				setSize(300,150);
+				setDefaultCloseOperation(EXIT_ON_CLOSE);
+				JPanel pan1= new JPanel();
+				JButton bouton = new JButton(" LOGIN ");
+				JTextField txuser = new JTextField(15);
+
+				pan1.add(bouton);
+				pan1.add(txuser);
+				this.setContentPane(pan1);
+				this.setVisible(true);
+				pan1.setVisible(true);
+				System.out.println("salut");
+			}
+		};
 	}
-};
 }
--- big boss band/src/Projet/Salle.java
+++ big boss band/src/Projet/Salle.java
@@ -3,4 +3,16 @@ package Projet;
 public class Salle {
 int idSalle, nombrePlace, nbPc, nbBureau, nbChaise, videoProj, tableau;
 
+
+public void Salle(int idSalle, int nombrePlace,int nbPc,int nbBureau,int nbChaise,int videoProj,int tableau){
+	this.idSalle=idSalle;
+	this.nombrePlace=nombrePlace;
+	this.nbPc=nbPc;
+	this.nbBureau=nbBureau;
+	this.nbChaise=nbChaise;
+	this.videoProj=videoProj;
+	this.tableau=tableau;
+}
+
+
 }
