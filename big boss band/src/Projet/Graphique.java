a9c29250214ece71e8bb0fa4abe789ff9cc790cd
allan <allan@10.0.2.15>
2017-01-16 10:07:54 +0100

    modif graphique

--- big boss band/src/Projet/Graphique.java
+++ big boss band/src/Projet/Graphique.java
@@ -1,6 +1,7 @@
 package Projet;
 
 import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.MouseEvent;
 
 import javax.swing.JButton;
@@ -10,17 +11,20 @@ import javax.swing.JPanel;
 import javax.swing.JTextField;
 
 public class Graphique extends JFrame{
-
+	
+	String mp, log;
+	JTextField txuser = new JTextField(15);
+	JTextField txuser2 = new JTextField(15);  
+	JPanel pan1= new JPanel();
+	JPanel pan2 = new JPanel();
+	JLabel login = new JLabel("LOGIN");
+	JLabel password = new JLabel("Password");
+	JButton connecte = new JButton ("Connection");
+	
 	public Graphique (){
-		JPanel pan1= new JPanel();
-		JPanel pan2 = new JPanel();
-		JLabel login = new JLabel("LOGIN");
-		JLabel password = new JLabel("Password"); 
-		JTextField txuser = new JTextField(15);
-		JTextField txuser2 = new JTextField(15);  
-		JButton connecte = new JButton ("Connection");
-		
 
+		
+		
 		setSize(250,150);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 		setResizable(false);
@@ -32,14 +36,29 @@ public class Graphique extends JFrame{
 		pan1.add(txuser2);
 		pan1.add(connecte);
 		
+
+		
 		this.setContentPane(pan1);
 		pan1.setVisible(true);
 		this.setVisible(true); 
+		
+		
 
 
 	}
+	class Connecte implements ActionListener{
+
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			log = txuser.getText();
+			mp = txuser2.getText();
+			
+		}
+		
+		
+	}
 	public void mouseClicked(MouseEvent arg0) {
-		// TODO Auto-generated method stub
+	
 
 	}
 
--- big boss band/src/Projet/Main.java
+++ big boss band/src/Projet/Main.java
@@ -3,7 +3,6 @@ package Projet;
 public class Main {
 
 	public static void main(String[] args) {
-
 		
 		Graphique JFrame = new Graphique();
 			
