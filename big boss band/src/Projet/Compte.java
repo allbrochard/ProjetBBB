ce508f1ea653f09a5a362fb455244b0cac9f1f29
FcS <FcS@10.0.2.15>
2017-01-16 15:25:07 +0100

    modif admin

--- big boss band/src/Projet/Compte.java
+++ big boss band/src/Projet/Compte.java
@@ -3,6 +3,7 @@ package Projet;
 public abstract class Compte {
 	String login, mp,nom,prenom;
 	int age;
+	protected int id;
 
 	public Compte(String login, String mp, String nom, String prenom, int age){
 		this.login=login;
--- big boss band/src/Projet/Main.java
+++ big boss band/src/Projet/Main.java
@@ -4,7 +4,10 @@ public class Main {
 
 	public static void main(String[] args) {
 		
-		Graphique JFrame = new Graphique();
+//		Graphique JFrame = new Graphique();
+		Admin adm = new Admin("admin", "admin", "Fett", "Tarti", 26);
+		
+	adm.creeCompte();
 		
 
 		}
