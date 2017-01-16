afb98d2788370011fa5dd60a732c2c79ab63ad72
allan <allan@10.0.2.15>
2017-01-16 09:47:53 +0100

    ajout connection

--- big boss band/src/Projet/Graphique.java
+++ big boss band/src/Projet/Graphique.java
@@ -2,6 +2,7 @@ package Projet;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.MouseEvent;
+
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -11,64 +12,63 @@ import javax.swing.JTextField;
 public class Graphique extends JFrame{
 
 	public Graphique (){
+		JPanel pan1= new JPanel();
+		JPanel pan2 = new JPanel();
+		JLabel login = new JLabel("LOGIN");
+		JLabel password = new JLabel("Password"); 
+		JTextField txuser = new JTextField(15);
+		JTextField txuser2 = new JTextField(15);  
+		JButton connecte = new JButton ("Connection");
+		
 
-	setSize(250,150);
-    setDefaultCloseOperation(EXIT_ON_CLOSE);
-    setResizable(false);
-    setLocationRelativeTo(null);
-    JPanel pan1= new JPanel();
-    JLabel login = new JLabel("LOGIN");
-    JTextField txuser = new JTextField(15);
+		setSize(250,150);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		setResizable(false);
+		setLocationRelativeTo(null);
 
-    JLabel password = new JLabel("Password"); 
-    JTextField txuser2 = new JTextField(15);  
-    JButton connecte = new JButton ("Connection");
-    pan1.add(login);
-    pan1.add(txuser);
-    this.setContentPane(pan1);
-    pan1.setVisible(true);
-    System.out.println("salut");
+		pan1.add(login);
+		pan1.add(txuser);
+		pan1.add(password);
+		pan1.add(txuser2);
+		pan1.add(connecte);
+		
+		this.setContentPane(pan1);
+		pan1.setVisible(true);
+		this.setVisible(true); 
 
-    pan1.add(password);
-    pan1.add(txuser2);
-    this.setContentPane(pan1);
-    pan1.setVisible(true);
-    pan1.add(connecte);
-    this.setVisible(true);
-	
-	
+
 	}
 	public void mouseClicked(MouseEvent arg0) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
-	
+
 	public void mouseEntered(MouseEvent arg0) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
-	
+
 	public void mouseExited(MouseEvent arg0) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
-	
+
 	public void mousePressed(MouseEvent arg0) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
-	
+
 	public void mouseReleased(MouseEvent arg0) {
 		// TODO Auto-generated method stub
-		
+
 	}
-	
+
 	public void actionPerformed(ActionEvent arg0) {
 		// TODO Auto-generated method stub
-		
-}
+
+	}
 }
