5522cf94fbfd21c8dc9b7c7275d17e264cb39917
allan <allan@allan-PC.home>
2017-01-16 21:40:24 +0100

    allignement

--- big boss band/src/projet/Graphique.java
+++ big boss band/src/projet/Graphique.java
@@ -27,16 +27,9 @@ public class Graphique extends JFrame{
 	public Graphique (){
 
 		login = new JLabel("LOGIN");
-		
 		password = new JLabel("Password");
-		
 		txuser = new JTextField(15);
-		
 		txuser2 = new JTextField(15); 
-		
-
-		
-		
 
 		setSize(250,150);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
@@ -58,7 +51,7 @@ public class Graphique extends JFrame{
 		pan1.setVisible(true);
 
 		this.setVisible(true); 
-		
+
 	}
 	class Connecte implements ActionListener{
 
