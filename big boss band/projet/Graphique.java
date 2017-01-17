65838ba6723b26aeeb943bc3c71d832ff24f2d22
allan <allan@10.0.2.15>
2017-01-17 09:43:06 +0100

    save

--- big boss band/projet/Graphique.java
+++ big boss band/projet/Graphique.java
@@ -1,67 +1,7 @@
 package projet;
-import java.awt.GridBagConstraints;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseEvent;
-<<<<<<< HEAD
-
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JOptionPane;
-import javax.swing.JPanel;
-import javax.swing.JTextField;
-
-public class Graphique extends JFrame{
-
-	String mp, log;
-
-	JTextField txuser = new JTextField(15);
-	JTextField txuser2 = new JTextField(15);  
-	JPanel pan1= new JPanel();
-	JPanel pan2 = new JPanel();
-	JLabel login = new JLabel("LOGIN");
-	JLabel password = new JLabel("Password");
-	JButton connecte = new JButton ("Connection");
-	String a="allan";
-	String b="allan";
-	public Graphique (){
-
-		login = new JLabel("LOGIN");
-		
-		password = new JLabel("Password");
-		
-		txuser = new JTextField(15);
-		
-		txuser2 = new JTextField(15); 
-		
-
-		
-		
-
-		setSize(250,150);
-		setDefaultCloseOperation(EXIT_ON_CLOSE);
-		setResizable(false);
-		setLocationRelativeTo(null);
-
-
-		pan1.add(login);
-		pan1.add(txuser);	
-		pan1.add(password);
-		pan1.add(txuser2);
-		pan1.add(connecte);
-
-
-		this.setContentPane(pan2);
-		pan2.setVisible(true);
-
-		this.setContentPane(pan1);
-		pan1.setVisible(true);
-
-		this.setVisible(true); 
-		
-	}
-=======
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
